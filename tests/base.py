7937c3fb66db4bbd7e73fa060735d5bfc978d18a
rusty <rusty@metricinsights.com>
2016-09-12 13:47:46 +0300

    Fix tests for new JJB API.

--- tests/base.py
+++ tests/base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from jenkins_jobs.builder import Builder
-from cStringIO import StringIO
+from jenkins_jobs.config import JJBConfig
+from jenkins_jobs.xml_config import XmlJobGenerator
+from jenkins_jobs.parser import YamlParser
+from jenkins_jobs.registry import ModuleRegistry
 import xml.etree.ElementTree as ET
 import os
 
@@ -10,9 +13,21 @@ BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def assert_case(case_name):
     case_source, case_result = (os.path.join(BASE_PATH, case_name + ext) for ext in ['.yml', '.xml'])
-    xml_output = StringIO()
-    builder = Builder('http://localhost:8080/', '', '', ignore_cache=True)
-    builder.update_jobs(case_source, output=xml_output, n_workers=1)
-    result_xml = ET.XML(xml_output.getvalue())
+    jjb_config = JJBConfig()
+    builder = Builder(jjb_config)
+
+    # Generate XML
+    parser = YamlParser(jjb_config)
+    registry = ModuleRegistry(jjb_config, builder.plugins_list)
+    xml_generator = XmlJobGenerator(registry)
+
+    parser.load_files(case_source)
+    registry.set_parser_data(parser.data)
+
+    job_data_list = parser.expandYaml(registry, [])
+
+    xml_jobs = xml_generator.generateXML(job_data_list)
+
+    result_xml = ET.XML(xml_jobs[0].output())
     expected_xml = ET.XML(open(case_result).read())
     assert ET.tostring(result_xml) == ET.tostring(expected_xml)
